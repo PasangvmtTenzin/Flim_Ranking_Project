827e7c9eda260ab491a292119d8d89339a1057fb
pasang <o5220173.jnec@rub.edu.bt>
2024-07-03 15:22:27 +0200

    letf to play with parametere

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,12 +2,5 @@ import pandas as pd
 import os
 
 def load_csv(file_path):
-    # Construct the absolute path using os.path.join
-    abs_file_path = os.path.join(os.path.dirname(__file__), file_path)
+    abs_file_path = os.path.join(os.path.dirname(__file__), '..', 'merged_data', file_path)
     return pd.read_csv(abs_file_path)
-
-# Example usage:
-data = load_csv('../merged_data/final_data.csv')
-
-
-
